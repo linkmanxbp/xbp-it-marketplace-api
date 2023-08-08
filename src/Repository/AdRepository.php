52d0a2aec53d88bf3a6e7e5f66d51a8e9c3d572b
linkmanxbp <linkman_xbp@yahoo.fr>
2023-08-08 16:35:53 +0200

    Symfony 6.3.3

--- src/Repository/AdRepository.php
+++ src/Repository/AdRepository.php
@@ -2,10 +2,17 @@
 
 namespace App\Repository;
 
+use App\Entity\Ad;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
 class AdRepository extends ServiceEntityRepository
 {
+    public function __construct(ManagerRegistry $registry, string $entityClass = Ad::class)
+    {
+        parent::__construct($registry, $entityClass);
+    }
+
     public function findEasy(string $query): array
     {
         return $this->createQueryBuilder('ad')
